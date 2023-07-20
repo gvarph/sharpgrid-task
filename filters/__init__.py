1ef080ec4640515686f6e25897dc7f6cdd530fd2
Filip Krul <gvarph006@gmail.com>
2023-07-20 22:26:48 +0200

    improved docstrings

--- filters/__init__.py
+++ filters/__init__.py
@@ -10,6 +10,17 @@ def get_possible_categories(
     lines_to_pages: dict[int, int],
     conf_threshold=0.75,  # randomly chosen valueba
 ) -> List[Line]:
+    """
+    Filters lines from the provided menu and returns lines with a high category confidence.
+
+    Args:
+        menu (Menu): The menu from which to filter lines.
+        lines_to_pages (dict[int, int]): A mapping from line numbers to page numbers.
+        conf_threshold (float, optional): The minimum category confidence required for a line to be returned. Default is 0.75.
+
+    Returns:
+        List[Line]: A list of lines with category confidence above the provided threshold.
+    """
     line_filter = LineFilter(
         FilterPriceLines(0.5),
         FilterLongLines(0.1, dropoff_start=6, reduction_per_word=0.2),
--- filters/base.py
+++ filters/base.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from typing import Tuple
 from scan_classes import *
 
 from logger import get_logger
@@ -7,7 +8,10 @@ logger = get_logger(__name__)
 
 
 class Filter(ABC):
-    """Abstract base class for filters with an abstract method `apply()`."""
+    """
+    Abstract base class for filters.
+    Each filter must implement an `apply` method which modifies a provided list of lines.
+    """
 
     def __init__(self, weight: float):
         """Initialize the Filter with a weight parameter."""
@@ -21,25 +25,27 @@ class Filter(ABC):
 
 class LineFilter:
     """
-    LineFilter is used to apply multiple filters on lines.
+    A filter that applies multiple sub-filters to a list of lines.
 
     Args:
-        filters (Filter): Filters to be applied.
+        filters (Filter): The filters to be applied to the lines.
     """
+
+    filters: Tuple[Filter]
 
     def __init__(self, *filters: Filter):
         self.filters = filters
 
     def get_possible_categories(self, menu: Menu, conf_threshold=0.76) -> List[Line]:
         """
-        Filters the lines and returns those with category_confidence above the conf_threshold.
+        Filters the lines from the provided menu and returns lines with category confidence above the provided threshold.
 
         Args:
-            menu (Menu): The menu that contains the lines to be filtered.
-            conf_threshold (float): The minimum category_confidence for a line to be considered.
+            menu (Menu): The menu from which to filter lines.
+            conf_threshold (float, optional): The minimum category confidence required for a line to be returned. Default is 0.75.
 
         Returns:
-            List[Line]: Lines with category_confidence greater than conf_threshold.
+            List[Line]: A list of lines with category confidence above the provided threshold.
         """
 
         def lines_above_confidence() -> List[Line]:
